0159bd6491e50b936c33508e91f0890895e00303
corysbruh <corytsan@gmail.com>
2023-07-21 23:41:46 -0700

    idk

--- gogScrape.py
+++ gogScrape.py
@@ -8,3 +8,11 @@ name = name.replace(" ","%20")
 website = urllib.request.urlopen("https://www.gog.com/en/games?query=" + name).read()
 soup = BeautifulSoup(website, "html.parser")
 print(website)
+
+for section in soup.find_all("a", class_='product-tile product-tile--grid'):
+    link = section.get("href")
+    checker = True
+    for i in gNameList:
+        if i.lower() not in link.lower():
+            checker = False
+
