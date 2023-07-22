411df40690516d560ce6c96da716a33c8239e590
ibmrt <xiqixian@gmail.com>
2023-07-21 21:17:41 -0700

    Update steamScrape.py

--- steamScrape.py
+++ steamScrape.py
@@ -20,8 +20,38 @@ for item in search_soup.find_all(attrs = {'data-gpnav' : 'item'}):
     hLink = item.get('href')
     checker = True
     for i in keywords:
-        if i not in hLink:
+        if hLink != None:
+            if i.lower() not in hLink.lower():
+                checker = False
+        else:
             checker = False
     if checker:
         linkList.append(hLink)
-        
+        for child in item.children:
+            try:
+                nameList.append(child.find('span', class_='title').contents[0])
+            except:
+                pass
+        for child in item.children:
+            try:
+                priceSec = child.find('div', class_='col search_discount_and_price responsive_secondrow')
+                if priceSec != None:
+                    if len(priceSec.contents) > 1:
+                        priceList.append(child.find('div', class_='discount_final_price').contents[0])
+                    else:
+                        priceList.append('Unavailable or Free')
+            except:
+                pass
+for link in linkList:
+    gamePage = urllib.request.urlopen(link).read()
+    pageSource = BeautifulSoup(gamePage, 'html.parser')
+    reqSec = pageSource.find('div', class_='game_area_sys_req sysreq_content active')
+    print('1')
+    try:
+        if len(reqSec) > 1:
+            print(type(reqSec.contents))
+            pass
+        else:
+            print('None')
+    except:
+        pass
