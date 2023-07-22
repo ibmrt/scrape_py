0baa3b061c7cd0afa9f2d4f0cc5c9f8c53a776e8
ibmrt <xiqixian@gmail.com>
2023-07-21 22:45:13 -0700

    Update gameObj.py

---
+++ gameObj.py
@@ -0,0 +1,12 @@
+class SteamGame:
+    def __init__(id, sName, sLink, sPrice, sSetupList):
+        id.name = sName
+        id.link = sLink
+        id.price = sPrice
+        id.setupList = sSetupList
+    def display_all(id, max):
+        distance = len(id.name) + 8
+        t_distance = max - (distance + len('Requirements:'))
+        print(id.name + '        Requirements:' + id.price.rjust(t_distance, ' '))
+        for i in id.setupList:
+            print(''.ljust(distance, ' ') + i)
