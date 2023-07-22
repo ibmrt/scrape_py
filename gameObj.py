de6d75d437be62ea667280b484ffb2cff46f6e32
ibmrt <xiqixian@gmail.com>
2023-07-22 00:56:50 -0700

    Update gameObj.py

--- gameObj.py
+++ gameObj.py
@@ -5,9 +5,9 @@ class SteamGame:
         id.price = sPrice
         id.setupList = sSetupList
     def display_all(id, max):
-        distance = len(id.name) + 8
+        distance = len(id.name) + 7
         t_distance = max - (distance + len('Requirements:'))
         print(id.name + '        Requirements:' + id.price.rjust(t_distance, ' '))
         for i in id.setupList:
-            print(''.ljust(distance, ' ') + i)
+            print(''.ljust(distance, ' '), i)
         print(''.ljust(max, '-'))
