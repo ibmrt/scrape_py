4b0014ffcf56008efe1fdf5a0fabd77f2f1fdf9f
ibmrt <xiqixian@gmail.com>
2023-07-22 00:34:34 -0700

    Update uniScrape.py

--- uniScrape.py
+++ uniScrape.py
@@ -3,8 +3,7 @@ from steamScrape import sScrape
 print('Program which displays information of games after a search by the game name\n'
 + '(Type \'40sec\' to exit)\nWhen entering game name, enter words within the official game name for better accuracy')
 while True:
-    input('Enter game name here (avoid using special characters, use space to separate keywords)\n(ex: counter Strike, hollow knight, Only up, TERRARIA):\n')
-    gameName = 'hollow knight'
+    gameName = input('Enter game name here (avoid using special characters, use space to separate keywords)\n(ex: counter Strike, hollow knight, Only up, TERRARIA):\n')
     if gameName in '40sec':
         break
     else:
@@ -14,6 +13,7 @@ while True:
             if len(i.name) > max_:
                 max_ = len(i.name)
         max_ += 80
+        print(''.ljust(max_, '-'))
         for i in sGameList:
             i.display_all(max_)
     input('Press to continue...')
